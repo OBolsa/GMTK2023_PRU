c61c974f121f0c82805eaa76937a6c3897851b36
OBolsa <vitor-hug@hotmail.com>
2023-07-07 17:59:23 -0300

    Player Movement And defaults

---
+++ GMTK2023_PRU/Assets/Systems/Player/PlayerMovementConfigs.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Configs/Movement")]
+public class PlayerMovementConfigs : ScriptableObject
+{
+    public float maxMovementSpeed;
+    public float impulseSpeed;
+    public float fallSpeed;
+}
